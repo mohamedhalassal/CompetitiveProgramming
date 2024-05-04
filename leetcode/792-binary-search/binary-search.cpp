55bcc8c2a770bb7670ee35c9cd5b03f2ad4bd864
Mohamed Ahmed <125523649+mohmedahalassal@users.noreply.github.com>
2024-05-05 01:08:27 +0300

    Time: 22 ms (86.65%) | Memory: 29.8 MB (66.32%) - LeetSync

---
+++ leetcode/792-binary-search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& num, int x) {
+         int s=0,e=num.size()-1,mid,ans=-1;
+    while(s<=e){
+        mid=(s+e)/2;
+        if(num[mid]==x){
+           ans=mid;
+           break;
+        }
+        else if(num[mid]>x){
+             e=mid-1;
+        }
+        else {
+            s=mid+1;
+        }
+
+    }
+    return ans;
+    }
+};
