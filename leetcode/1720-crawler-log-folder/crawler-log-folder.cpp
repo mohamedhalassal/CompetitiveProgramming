2da8b5b57d9403f2eaa4c3edf63db773be5966f9
Mohamed Ahmed <125523649+mohmedahalassal@users.noreply.github.com>
2024-07-10 10:25:36 +0300

    Time: 3 ms (86.11%) | Memory: 14.2 MB (31.02%) - LeetSync

---
+++ leetcode/1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        for (int i = 0; i < logs.size(); i++) {
+            if(logs[i]=="../"){
+                if(!empty(st))st.pop();
+            }
+            else if(logs[i]=="./"){
+                continue;
+            }
+            else {
+                st.push(logs[i]);
+            }
+        }
+        return st.size();
+    }
+};
