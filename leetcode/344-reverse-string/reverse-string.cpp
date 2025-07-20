ba91669e773c3eed7b2d7b9f48c9506c1adb6e49
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2025-07-20 04:48:49 +0300

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.42%) - LeetSync

--- leetcode/344-reverse-string/reverse-string.cpp
+++ leetcode/344-reverse-string/reverse-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
-        for(int i=0;i<s.size();i++)
-        cout<<s[i];
+        int start = 0, end = s.size() - 1;
+        while (start < end)
+            swap(s[start++], s[end--]);
     }
 };
