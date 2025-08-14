f2e4f6f720689148e385f7e5c29013a98588558e
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2025-08-14 15:34:41 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (20.69%) - LeetSync

--- leetcode/80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ leetcode/80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int write = 0, i = 1;
-        map<int, int> mp;
-        mp[nums[0]]++;
-        while (i < nums.size()) {
-            if (nums[i] != nums[write] || mp[nums[i]] < 2) {
-                nums[++write] = nums[i];
-                mp[nums[i]]++;
+        int write = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (write < 2 || nums[i] != nums[write - 2]) {
+                nums[write++] = nums[i];
             }
-            i++;
         }
-        return write + 1;
+        return write;
     }
 };
