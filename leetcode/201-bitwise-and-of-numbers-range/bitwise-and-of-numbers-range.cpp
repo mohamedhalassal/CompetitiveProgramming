99d46b586dce28514ba3e806e60b8d1d25f0b032
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2025-09-13 03:04:50 +0300

    Time: 0 ms (100.00%) | Memory: 11.2 MB (80.37%) - LeetSync

--- leetcode/201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ leetcode/201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-      while(left<right){
-        right&=right-1;
-      }
-      return right;
+        int shift = 0;
+        while (left < right) {
+            right >>= 1;//decrese rigth to left
+            left >>= 1;//delete any ones and zeros outside rigth after delete bit in this
+            shift++;//count sifts to restore the common position
+        }
+        left <<= shift;
+        return left;
     }
 };
