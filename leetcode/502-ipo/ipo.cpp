2f25e798d6569c013d210e4a53d52de8f1693b75
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2025-09-18 07:28:51 +0300

    Time: 136 ms (13.50%) | Memory: 135.6 MB (17.04%) - LeetSync

--- leetcode/502-ipo/ipo.cpp
+++ leetcode/502-ipo/ipo.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits,vector<int>& capital) {
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         int n = profits.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>>
-            pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>>pq;
         priority_queue<pair<int, int>> mxp;
         for (int i = 0; i < n; i++) {
             pq.push({capital[i], profits[i]});
@@ -13,10 +12,10 @@ public:
                 mxp.push({pq.top().second, pq.top().first});
                 pq.pop();
             }
-            if (!empty(mxp)) {
-                w += mxp.top().first;
-                mxp.pop();
-            }
+            if (empty(mxp))
+                return w;
+            w += mxp.top().first;
+            mxp.pop();
         }
         return w;
     }
