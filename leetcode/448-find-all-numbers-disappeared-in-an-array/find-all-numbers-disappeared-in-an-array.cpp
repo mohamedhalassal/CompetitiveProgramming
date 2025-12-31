26be33cfad1b2c5b6853e76976ac42aa47990309
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2025-12-31 16:32:35 +0200

    Time: 0 ms (100.00%) | Memory: 54.9 MB (40.29%) - LeetSync

---
+++ leetcode/448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans, freq(n + 1, 0);
+        for (auto it : nums) {
+            freq[it]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 0)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
