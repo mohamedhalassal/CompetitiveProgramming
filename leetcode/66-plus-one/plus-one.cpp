445c5e45347f8756cfa25036c2777c95dc6e9393
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-01-01 11:41:07 +0200

    Time: 0 ms (100.00%) | Memory: 11.4 MB (77.72%) - LeetSync

--- leetcode/66-plus-one/plus-one.cpp
+++ leetcode/66-plus-one/plus-one.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        if (digits.back() != 9) {
-            digits.back()++;
-            return digits;
-        }
-        int i = digits.size() - 1;
-        for (; i >= 0; i--) {
-            if (digits[i] == 9)
-                digits[i] = 0;
-            else
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = (i == 0) ? 1 : 0;
+                if (i == 0)
+                    digits.push_back(0);
+            } else {
+                digits[i]++;
                 break;
-        }
-        if (i >= 0)
-            digits[i]++;
-        else {
-            digits[i + 1]++;
-            digits.push_back(0);
+            }
         }
         return digits;
     }
