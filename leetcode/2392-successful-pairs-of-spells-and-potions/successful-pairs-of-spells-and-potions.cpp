915f4a6e95e792ca75c603ff13382d2ff33271bb
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-03-10 13:32:43 +0200

    Time: 46 ms (43.13%) | Memory: 128.2 MB (53.86%) - LeetSync

--- leetcode/2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ leetcode/2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -4,10 +4,7 @@ public:
         vector<int> ans;
         int psz = potions.size();
         sort(potions.begin(), potions.end());
-        // i take the spell and going to check it
         for (int i = 0; i < spells.size(); i++) {
-            // i going to search to the the position of firs number is \leq the
-            // success
             int l = 0, r = psz - 1, mid, an = psz;
             while (l <= r) {
                 mid = (l + r) / 2;
@@ -18,23 +15,8 @@ public:
                     l = mid + 1;
                 }
             }
-            ans.push_back( psz - an);
+            ans.push_back(psz - an);
         }
         return ans;
     }
 };
-
-/*
-[1,2,3,4,5]   [5,1,3]
-
-2 8 1 3 4     4  2 8   sucsess = 10
-1 2 3 4 8
-4 8 12 16 32
-
-i searching of the first number i mult this to the spell number is a greter than
-or equel the sucsess and get the idx and substact this to the size of the
-potions
-
-i runing in all spells is a o(n) and binary search of potions in o(log n) --->
-o(n log n)
-*/
