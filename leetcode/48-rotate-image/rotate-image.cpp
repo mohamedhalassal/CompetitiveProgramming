aab37c0d112185949e8b5fbec9686eeb3bd2c7e3
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-05-04 13:29:16 +0300

    Time: 0 ms (100.00%) | Memory: 10.3 MB (35.34%) - LeetSync

--- leetcode/48-rotate-image/rotate-image.cpp
+++ leetcode/48-rotate-image/rotate-image.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> rot;
         for (int i = 0; i < n; i++) {
             vector<int> rot2;
-            for (int j = n - 1; j >= 0; j--) {
-                rot2.push_back(matrix[j][i]);
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
-            rot.push_back(rot2);
         }
-        matrix = rot;
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
