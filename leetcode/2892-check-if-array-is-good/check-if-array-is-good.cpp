20422de96727bfa68c2569766f7a74c2c6a7c06f
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-05-14 12:08:00 +0300

    Time: 0 ms (100.00%) | Memory: 27.3 MB (31.27%) - LeetSync

--- leetcode/2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ leetcode/2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -3,10 +3,10 @@ public:
     bool isGood(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> freq(nums[n - 1] + 1);
-        for (auto it : nums){
+        int freq[201] = {0};
+        for (auto it : nums) {
             freq[it]++;
-            if(it!=nums.back()&&freq[it]>1){
+            if (it != nums.back() && freq[it] > 1) {
                 return false;
             }
         }
