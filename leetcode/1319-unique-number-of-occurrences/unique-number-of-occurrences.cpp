de51c8b2409eaeed7e14b65d80170b4df69c9678
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-06-04 11:45:12 +0300

    Time: 0 ms (100.00%) | Memory: 11.9 MB (82.11%) - LeetSync

--- leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map<int, int> mp, check;
-        for (auto it : arr)
-            mp[it]++;
-        for (auto it : mp)
-            check[it.second]++;
-        for (auto it : check)
-            if (it.second > 1)
+        map<int, int> mp, mp2;
+        for (int i = 0; i < arr.size(); i++) {
+            mp[arr[i]]++;
+        }
+        for (auto it : mp) {
+            mp2[it.second]++;
+            if (mp2[it.second] > 1)
                 return false;
+        }
         return true;
     }
 };
