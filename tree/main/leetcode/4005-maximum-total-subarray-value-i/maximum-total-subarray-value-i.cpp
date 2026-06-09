b19334150fd2aabbc781f8047c2ac00fbdbaee9d
Mohamed Ahmed <125523649+mohamedhalassal@users.noreply.github.com>
2026-06-09 16:01:33 +0300

    Delete tree/main/leetcode/4005-maximum-total-subarray-value-i directory

--- tree/main/leetcode/4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    long long maxTotalValue(vector<int>& nums, int k) {
-        long long ans = *max_element(nums.begin(), nums.end()) -
-                        *min_element(nums.begin(), nums.end());
-        return (ans * k);
-    }
-};
